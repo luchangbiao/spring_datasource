cc659143e872162c93e43d772e694b3fb303fee2
cblu <Lcb8445741>
2021-07-30 16:46:58 +0800

    注册登录,手机号码验证

--- imooc-news-dev/imooc-news-dev-service-api/src/main/java/com/imooc/api/BaseController.java
+++ imooc-news-dev/imooc-news-dev-service-api/src/main/java/com/imooc/api/BaseController.java
@@ -2,6 +2,12 @@ package com.imooc.api;
 
 import com.imooc.utils.RedisOperator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BaseController {
 
@@ -9,6 +15,23 @@ public class BaseController {
     public RedisOperator redis;
 
     public static final String MOBILE_SMSCODE = "mobile:smscode";
+
+    /**
+     * 获取BO中的错误信息
+     * @param result
+     */
+    public Map<String, String> getErrors(BindingResult result) {
+        Map<String, String> map = new HashMap<>();
+        List<FieldError> errorList = result.getFieldErrors();
+        for (FieldError error : errorList) {
+            // 发送验证错误的时候所对应的某个属性
+            String field = error.getField();
+            // 验证的错误消息
+            String msg = error.getDefaultMessage();
+            map.put(field, msg);
+        }
+        return map;
+    }
 
 
 }
--- imooc-news-dev/imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/PassportController.java
+++ imooc-news-dev/imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/PassportController.java
@@ -3,15 +3,21 @@ package com.imooc.user.controller;
 import com.imooc.api.BaseController;
 import com.imooc.api.controller.user.PassportControllerApi;
 import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.grace.result.ResponseStatusEnum;
+import com.imooc.pojo.bo.RegistLoginBO;
 import com.imooc.utils.IPUtil;
 import com.imooc.utils.MyInfo;
 import com.imooc.utils.SMSUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.Map;
 
 @RestController
 public class PassportController extends BaseController implements PassportControllerApi {
@@ -30,7 +36,32 @@ public class PassportController extends BaseController implements PassportContro
         // 生成随机验证码并且发送短信
         String random = (int) ((Math.random() * 9 + 1) * 100000) + "";
 
+        // 把验证码存入redis，用于后续进行验证
+        redis.set(MOBILE_SMSCODE + ":" + mobile, random, 30 * 60);
+
         smsUtils.sendSMS(MyInfo.getMobile(), random);
+        return GraceJSONResult.ok();
+    }
+
+
+    @Override
+    public GraceJSONResult doLogin(@Valid RegistLoginBO registLoginBO,
+                                   BindingResult result) {
+
+        // 0.判断BindingResult中是否保存了错误的验证信息，如果有，则需要返回
+        if (result.hasErrors()) {
+            Map<String, String> map = getErrors(result);
+            return GraceJSONResult.errorMap(map);
+        }
+
+        String mobile = registLoginBO.getMobile();
+        String smsCode = registLoginBO.getSmsCode();
+
+        // 1. 校验验证码是否匹配
+        String redisSMSCode = redis.get(MOBILE_SMSCODE + ":" + mobile);
+        if (StringUtils.isBlank(redisSMSCode) || !redisSMSCode.equalsIgnoreCase(smsCode)) {
+            return GraceJSONResult.errorCustom(ResponseStatusEnum.SMS_CODE_ERROR);
+        }
         return GraceJSONResult.ok();
     }
 
