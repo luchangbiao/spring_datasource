bad680cba1b5705c3ab6654fce9fd8ea5d6a3959
cblu <Lcb8445741>
2021-07-30 11:11:33 +0800

    短信验证码生成，前后端联调跨域问题

--- imooc-news-dev/imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/PassportControllerApi.java
+++ imooc-news-dev/imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/PassportControllerApi.java
@@ -5,6 +5,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Api(value = "用户注册登录", tags = {"用户注册登录的controller"})
 @RequestMapping("passport")
@@ -12,7 +15,7 @@ public interface PassportControllerApi {
 
     @ApiOperation(value = "获得短信验证码", notes = "获得短信验证码", httpMethod = "GET")
     @GetMapping("/getSMSCode")
-    public GraceJSONResult getSMSCode();
+    public GraceJSONResult getSMSCode(@RequestParam String mobile, HttpServletRequest request);
 
 
 }
--- imooc-news-dev/imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/PassportController.java
+++ imooc-news-dev/imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/PassportController.java
@@ -1,7 +1,9 @@
 package com.imooc.user.controller;
 
+import com.imooc.api.BaseController;
 import com.imooc.api.controller.user.PassportControllerApi;
 import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.utils.IPUtil;
 import com.imooc.utils.MyInfo;
 import com.imooc.utils.SMSUtils;
 import org.slf4j.Logger;
@@ -9,15 +11,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
-public class PassportController implements PassportControllerApi {
+public class PassportController extends BaseController implements PassportControllerApi {
 
     final static Logger logger = LoggerFactory.getLogger(PassportController.class);
     @Autowired
     private SMSUtils smsUtils;
+
     @Override
-    public GraceJSONResult getSMSCode() {
-        String random = "123456";
+    public GraceJSONResult getSMSCode(String mobile, HttpServletRequest request) {
+        // 获得用户ip
+        String userIp = IPUtil.getRequestIp(request);
+        // 根据用户的ip进行限制，限制用户在60秒内只能获得一次验证码
+        redis.setnx60s(MOBILE_SMSCODE + ":" + userIp, userIp);
+
+        // 生成随机验证码并且发送短信
+        String random = (int) ((Math.random() * 9 + 1) * 100000) + "";
+
         smsUtils.sendSMS(MyInfo.getMobile(), random);
         return GraceJSONResult.ok();
     }
