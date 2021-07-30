4b8224d02be431f1d09270ff805585cc049bd70d
cblu <Lcb8445741>
2021-07-30 23:36:26 +0800

    登录成功后将用户信息和token储存在cookie中

--- imooc-news-dev/imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/PassportControllerApi.java
+++ imooc-news-dev/imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/PassportControllerApi.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Api(value = "用户注册登录", tags = {"用户注册登录的controller"})
@@ -21,6 +22,8 @@ public interface PassportControllerApi {
     @ApiOperation(value = "一键注册登录接口", notes = "一键注册登录接口", httpMethod = "POST")
     @PostMapping("/doLogin")
     public GraceJSONResult doLogin(@RequestBody @Valid RegistLoginBO registLoginBO,
-                                   BindingResult result);
+                                   BindingResult result,
+                                   HttpServletRequest request,
+                                   HttpServletResponse response);
 
 }
--- imooc-news-dev/imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/PassportController.java
+++ imooc-news-dev/imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/PassportController.java
@@ -9,6 +9,7 @@ import com.imooc.pojo.AppUser;
 import com.imooc.pojo.bo.RegistLoginBO;
 import com.imooc.user.service.UserService;
 import com.imooc.utils.IPUtil;
+import com.imooc.utils.JsonUtils;
 import com.imooc.utils.MyInfo;
 import com.imooc.utils.SMSUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -19,8 +20,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class PassportController extends BaseController implements PassportControllerApi {
@@ -51,7 +54,9 @@ public class PassportController extends BaseController implements PassportContro
 
     @Override
     public GraceJSONResult doLogin(@Valid RegistLoginBO registLoginBO,
-                                   BindingResult result) {
+                                   BindingResult result,
+                                   HttpServletRequest request,
+                                   HttpServletResponse response) {
 
         // 0.判断BindingResult中是否保存了错误的验证信息，如果有，则需要返回
         if (result.hasErrors()) {
@@ -76,7 +81,25 @@ public class PassportController extends BaseController implements PassportContro
             // 如果用户没有注册过，则为null，需要注册信息入库
             user = userService.createUser(mobile);
         }
-        return GraceJSONResult.ok();
+
+
+        // 3. 保存用户分布式会话的相关操作
+        int userActiveStatus = user.getActiveStatus();
+        if (userActiveStatus != UserStatus.FROZEN.type) {
+            // 保存token到redis
+            String uToken = UUID.randomUUID().toString();
+            redis.set(REDIS_USER_TOKEN + ":" + user.getId(), uToken);
+            redis.set(REDIS_USER_INFO + ":" + user.getId(), JsonUtils.objectToJson(user));
+
+            // 保存用户id和token到cookie中
+            setCookie(request, response, "utoken", uToken, COOKIE_MONTH);
+            setCookie(request, response, "uid", user.getId(), COOKIE_MONTH);
+        }
+
+        // 4. 用户登录或注册成功以后，需要删除redis中的短信验证码，验证码只能使用一次，用过后则作废
+        redis.del(MOBILE_SMSCODE + ":" + mobile);
+        // 5. 返回用户状态
+        return GraceJSONResult.ok(userActiveStatus);
     }
 
 
