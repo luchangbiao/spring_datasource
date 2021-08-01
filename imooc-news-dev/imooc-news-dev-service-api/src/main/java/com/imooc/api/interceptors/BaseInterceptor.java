fcd239e0a71b3497dd313c1bc5ff2b614ae54216
cblu <Lcb8445741>
2021-08-01 15:46:24 +0800

    在更新用户信息和查询账号信息前加上token校验

---
+++ imooc-news-dev/imooc-news-dev-service-api/src/main/java/com/imooc/api/interceptors/BaseInterceptor.java
@@ -0,0 +1,42 @@
+package com.imooc.api.interceptors;
+
+import com.imooc.exception.GraceException;
+import com.imooc.grace.result.ResponseStatusEnum;
+import com.imooc.utils.RedisOperator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BaseInterceptor {
+
+    @Autowired
+    public RedisOperator redis;
+
+    public static final String REDIS_USER_TOKEN = "redis_user_token";
+    public static final String REDIS_USER_INFO = "redis_user_info";
+    public static final String REDIS_ADMIN_TOKEN = "redis_admin_token";
+
+    public boolean verifyUserIdToken(String id,
+                                     String token,
+                                     String redisKeyPrefix) {
+
+        if (StringUtils.isNotBlank(id) && StringUtils.isNotBlank(token)) {
+            String redisToken = redis.get(redisKeyPrefix + ":" + id);
+            if (StringUtils.isBlank(id)) {
+                GraceException.display(ResponseStatusEnum.UN_LOGIN);
+                return false;
+            } else {
+                if (!redisToken.equalsIgnoreCase(token)) {
+                    GraceException.display(ResponseStatusEnum.TICKET_INVALID);
+                    return false;
+                }
+            }
+        } else {
+            GraceException.display(ResponseStatusEnum.UN_LOGIN);
+            return false;
+        }
+
+        return true;
+    }
+
+
+}
