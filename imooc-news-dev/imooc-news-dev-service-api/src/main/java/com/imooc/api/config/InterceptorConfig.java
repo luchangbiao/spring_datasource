4dbaa5250e74bb56b83e5bd21eb048ae9a36d96d
cblu <Lcb8445741>
2021-09-11 15:29:15 +0800

    友情链接的增删改查-mongodb

--- imooc-news-dev/imooc-news-dev-service-api/src/main/java/com/imooc/api/config/InterceptorConfig.java
+++ imooc-news-dev/imooc-news-dev-service-api/src/main/java/com/imooc/api/config/InterceptorConfig.java
@@ -50,6 +50,9 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .addPathPatterns("/adminMng/addNewAdmin")
                 .addPathPatterns("/adminMng/getAdminList")
                 .addPathPatterns("/fs/uploadToGridFS")
-                .addPathPatterns("/fs/readInGridFS");
+                .addPathPatterns("/fs/readInGridFS")
+                .addPathPatterns("/friendLinkMng/saveOrUpdateFriendLink")
+                .addPathPatterns("/friendLinkMng/getFriendLinkList")
+                .addPathPatterns("/friendLinkMng/delete");
     }
 }
