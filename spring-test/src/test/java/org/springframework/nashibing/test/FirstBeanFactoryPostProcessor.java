1454c60e8849a5d0f1c3a8c0e1b6274cc81e493a
l_cb@winning.com.cn <lcb@8445741>
2024-10-21 21:45:11 +0800

    BeanFactoryPostProcessor 执行顺序调整

--- spring-test/src/test/java/org/springframework/nashibing/test/FirstBeanFactoryPostProcessor.java
+++ spring-test/src/test/java/org/springframework/nashibing/test/FirstBeanFactoryPostProcessor.java
@@ -4,13 +4,19 @@ package org.springframework.nashibing.test;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
-@Order(1)
-public class FirstBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+public class FirstBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
 
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
 		System.out.println("测试BeanFactoryPostProcessor 扩展  ====first");
+	}
+
+	@Override
+	public int getOrder() {
+		return 2;
 	}
 }
--- spring-test/src/test/java/org/springframework/nashibing/test/SecondBeanFactoryPostProcessor.java
+++ spring-test/src/test/java/org/springframework/nashibing/test/SecondBeanFactoryPostProcessor.java
@@ -4,13 +4,19 @@ package org.springframework.nashibing.test;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
 @Order(2)
-public class SecondBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+public class SecondBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
 
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
 		System.out.println("测试BeanFactoryPostProcessor ====second");
+	}
+
+	@Override
+	public int getOrder() {
+		return 1;
 	}
 }
